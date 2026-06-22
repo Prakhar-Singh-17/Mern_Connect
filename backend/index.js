1344cb0d29a3075213d9a7b892de5dfa42c9a115
Prakhar Singh <iamprakharsingh@gmail.com>
2026-06-22 07:04:18 +0530

    Changed cookie

--- backend/index.js
+++ backend/index.js
@@ -20,12 +20,7 @@ app.use(cors({
     credentials: true
 }));
 app.use(express.json());
-res.cookie("token", token, {
-  httpOnly: true,
-  secure: process.env.NODE_ENV === "production",
-  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-  maxAge: 7 * 24 * 60 * 60 * 1000,
-});
+app.use(express.urlencoded({ extended: true }));
 
 
 app.use("/user",userRoute);
